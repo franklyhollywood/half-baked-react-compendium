9c5d491bd8d9ced51de5f3a784d5430b127072d6
Frank Hollywood <github@srvsync.com>
2021-12-02 21:18:42 -0800

    fixed the if statement not needed on line 22

--- src/views/Compendium/Compendium.jsx
+++ src/views/Compendium/Compendium.jsx
@@ -18,16 +18,15 @@ export default function Compendium() {
 	const [selectedType, setSelectedType] = useState('all');
 
 	//TODO 😖 help!
-	if (pokemons.count !== 0) {
-		useEffect(() => {
-			const getPokemon = async () => {
-				const pokemonList = await fetchPokemon();
-				setPokemons(pokemonList);
-				setLoading(false);
-			};
-			getPokemon();
-		}, []);
-	}
+
+	useEffect(() => {
+		const getPokemon = async () => {
+			const pokemonList = await fetchPokemon();
+			setPokemons(pokemonList);
+			setLoading(false);
+		};
+		getPokemon();
+	}, []);
 
 	//TODO 😖 help!
 	useEffect(() => {
